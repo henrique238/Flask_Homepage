ca3640567eabf34b6ca28345fe5c762b6d4e399f
henrique238 <henriique_bm@hotmail.com>
2021-09-10 00:00:33 -0300

    Pushing clean project

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
-Successfully created project 'Flask' on GitHub, but initial commit
-failed: Author identity unknown *** Please tell me who you are.
-Run git config --global user.email "you@example.com" git config --global user.name "Your Name" to set your account's default identity. Omit --global to set the identity only in this repository. unable to auto-detect email address (got 'henri@LAPTOP-41K7QUHN.(none)')
+from flask import Flask, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello! This is the main page <h1>Hello<h1>"
+
+@app.route("/<name>")
+def user(name):
+    return f"Hello {name}!"
+
+@app.route("/admin")
+def admin():
+    return redirect (url_for("home"))
+
+if __name__ == "__main__":
+    app.run()
