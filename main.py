1de5c528f5a0b1617ae038fa43534f6b592e3eb1
henrique238 <henriique_bm@hotmail.com>
2021-09-09 23:49:04 -0300

    Pushing clean project

---
+++ main.py
@@ -0,0 +1,3 @@
+Successfully created project 'Flask' on GitHub, but initial commit
+failed: Author identity unknown *** Please tell me who you are.
+Run git config --global user.email "you@example.com" git config --global user.name "Your Name" to set your account's default identity. Omit --global to set the identity only in this repository. unable to auto-detect email address (got 'henri@LAPTOP-41K7QUHN.(none)')
